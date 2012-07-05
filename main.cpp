281f6e2bf1ac7215944b8f4ade492fc4f216df82
Michael James <michael.james@tufts.edu>
2012-07-05 11:49:33 -0400

    learning

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ Circ_list* clist_ptr;
 
 int main(int argc, char* argv[])
 {
-	if(argc < 2)
+	if(argc < 3)
 	{
 		cout << "This program takes in 2 parameters: the start vertex number ";
 		cout << "and then the location of the graph file.  The 1st vertex is ";
